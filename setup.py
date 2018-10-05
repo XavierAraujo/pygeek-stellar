a3908c84334e8b0387a8da59516b299b8e04dbc0
Xavier Araújo <xa@hypelabs.io>
2018-10-05 17:03:34 +0100

    Added setup.py file. Changed source code folder name to be according to the package naming convention: pygeek_stellar instead of pygeek-stellar

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pygeek-stellar',
+      version='0.1',
+      description='A Python CLI to interact with the Stellar network',
+      url='https://github.com/XavierAraujo/pygeek-stellar',
+      author='Xavier Araújo',
+      author_email='xavier.araujo92@gmail.com',
+      license='MIT',
+      packages=['pygeek_stellar'],
+      zip_safe=False)
