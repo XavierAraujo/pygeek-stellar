d688c7e647956eb9b6ca0b3ad250f8cd655d4390
Xavier Araújo <xa@hypelabs.io>
2018-10-09 11:02:08 +0100

    Added test file for stellar utilities module

---
+++ test/pygeek_stellar/utils/stellar_test.py
@@ -0,0 +1,25 @@
+import unittest
+from pygeek_stellar.utils.stellar import *
+
+
+class StellarTest(unittest.TestCase):
+
+    def test_is_valid_public_key(self):
+        pass
+        # TODO
+
+    def test_is_valid_private_key(self):
+        pass
+        # TODO
+
+    def test_is_valid_transaction_text_memo(self):
+        pass
+        # TODO
+
+    def test_is_valid_keypair(self):
+        pass
+        # TODO
+
+
+if __name__ == '__main__':
+    unittest.main()
