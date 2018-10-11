fb3f7a72aa16ec885114638344d02859fcff6649
Xavier Araújo <xa@hypelabs.io>
2018-10-11 17:08:43 +0100

    moved main function to new module name pygeek_stellar.py. Updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(name='pygeek-stellar',
       version='0.1',
-      description='A Python CLI to interact with the Stellar network',
+      description='A Python CLI to interact with the Stellar network.',
       url='https://github.com/XavierAraujo/pygeek-stellar',
-      author='XavierAraujo',
+      author='Xavier Araújo',
       author_email='xavier.araujo92@gmail.com',
       license='MIT',
       packages=['pygeek_stellar', 'pygeek_stellar.utils'],
